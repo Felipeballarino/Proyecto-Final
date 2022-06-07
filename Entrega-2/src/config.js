a2b8ac62c533570ceb71982f399ce855d1db2610
Felipeballarino <Felipeballarino97@gmail.com>
2022-06-07 19:14:42 -0300

    Entrega 2

---
+++ Entrega-2/src/config.js
@@ -0,0 +1,26 @@
+export default {
+    fileSystem: {
+        path: './DB'
+    },
+    mongodb: {
+        cnxStr: 'mongodb://localhost/ecommerce',
+        options: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            serverSelectionTimeoutMS: 5000,
+        }
+    },
+    firebase: {
+        "type": "service_account",
+        "project_id": "felipedata-89e41",
+        "private_key_id": "83ef002f2437da04ff8a09f4e0f757ca3aaf9439",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCm4VrHbdeU52t3\nbvDJPPnAH7qQohQa6F6bQ9zCM1ZcnsdMafy4EluqVPWvlRlO2pUwNfoyEnVSYUUe\nvgLeX9qirfBR/jslTQfiu1q5ZCVXLtEbvwmtVW/wrPXHh9wS1fcfiUyOSeDfyqbU\nAadyU7Uaccz7h6SCx+rAXGfxMbtef/KSyM04Xqzg8kEK+z7XeYG+MIAj3M09fe6N\nGT2pJI+8IF3oF28doHGC6WRKHf2rOx3GE8dwN/OcmckhEVFmW385DNhSWrNn3xXk\ntpUv4fTojoyW49ZdjGMq2Gw5fUHpzJU2j268DUMgbnXcsObXBPVW5bE+RovORDtU\nW8GsLxOZAgMBAAECggEAB+nep/kufiVg2qYMWkHY+hWT5eRTUPKJ+5G2EanlKexu\nbDihpYnBU3KtJk/LIAq/hMZagGHEGU1hilN3LijPfmPPMwqVe9XxgZZiEMtawsEG\nmFE9c6NkjosB9x20zI128/jkneHxyjFnxEMonjd+YN9BDQB7enQugd0f7lEhOICD\n7kyudpYAF/c7j84nsKwTQfZkG8DFK+7nb0+XEGqG+awgV1cT/n0NhU0oiVQl0US7\n2RvFpB751tmodeqCQKYniLRNlZQAOJ6y8W5TIlsRvzXL5BiOvDCarZ38OXd6pGDl\nWzbYmiMIsVQXcwV+9yp0d7zev1jXu2LrccaTtm2urQKBgQDZ4zpNRQ32rSHk2xo3\nj2JeWDztOxZJzlaJijxn0OehlPeR/XgevuWUM0S3veAs57UZvK4zBsaxoAozDZXA\n5pAiw0xzCloz4sJ+tfQ9ayc4BrKxRysONKMEXjQrjI3dugvNiRnUoHNmY45Weeke\n1CHWA6xtnC82vpJDHlfddZdp/wKBgQDEEhL+AlW/FcUpzek1YCE5/5cctUTlmSh3\n0vPFJ8MRXABy7mjvu3p/RwU+1fVuIOfugDXjw0LNbRMtRC8wwHkv13ntF4Bwxf7b\nWBrhcNLZKaj9s/Mh8309zIEstDVZr/sF+clO991OoV0zPrjKWHixdME5AW5P4sft\ngtW43jCSZwKBgQDDs8R6Arw4A+mdlO5HWRIRi0+uBJRQVnSDtRXsaUajaxmylud9\nNhkes63VG0ZvDu8q3r3YNw3gHM8Ma/dF+9EaaDASZAoA8HuhM+zb/ZKMT/fQIwwZ\nBk4lbjeNpgEGVkSjp7D7ueHs5R7DrVwlxINfpo0JJm+BzEmy314sG6KUIQKBgQC1\nMU7D1jyPffraiyN84/1Qr9Zh/VJSK5uuQBBunjQykqcHjzMCeGrXvDYov2k0AMzV\nJz6l4QiW9rnGF0DU+MoFYEeYxyM3FIJVCbKtq6nubgVxjx6cvXt75MVrHo4+99Yu\ny4sxovWteIm8lbYHqILA23jEyYG76YOBBmujkBGq+QKBgBv697JhgvV70AhqxA84\nfQG2O46nDqoBQ+0BWIGr7HCxhKn/rmO+pfCM3gRWiSNpQEsdqDMkhO7FYyaxqNzu\nl4xojD5SIxwcd2dxL6SECMUuaVvUxxJm9/HHXGH4Vad6f1uLWK/uCA7dVQgJuBcF\nfN3hQKvM78F2d+y06OlgLZ7T\n-----END PRIVATE KEY-----\n",
+        "client_email": "firebase-adminsdk-e4ahg@felipedata-89e41.iam.gserviceaccount.com",
+        "client_id": "113510519374549345173",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-e4ahg%40felipedata-89e41.iam.gserviceaccount.com"
+      }
+}
